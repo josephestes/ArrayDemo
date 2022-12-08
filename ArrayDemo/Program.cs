22e5f38de4fe395381c256bb78635969b7c96c6e
Joseph <54252110+josephestes@users.noreply.github.com>
2022-12-07 22:22:35 -0500

    Update Program.cs
    This optimization uses a foreach loop to display the array in its original order, which is more concise than using a for loop. In addition, the int.TryParse() method is replaced with the int.Parse() method, which is simpler and more intuitive. I also changed the Write() statements to WriteLine() statements to improve readability. Finally, I have added some basic input validation to the code so that it does not crash when the user enters an invalid position number.

--- ArrayDemo/Program.cs
+++ ArrayDemo/Program.cs
@@ -17,46 +17,54 @@ namespace ArrayDemo
         {
             const int EXIT = 4;
             int[] numbers = { 7, 6, 3, 2, 10, 8, 4, 5, 9, 1 };
-            int index;
             int userInput = 0;
 
-            do //run loop once, then check for sentinel value
+            do // run loop once, then check for sentinel value
             {
-                switch (userInput) //run code according to user input
+                switch (userInput) // run code according to user input
                 {
-                    case 1: //Display array in original order
-                        Write("View list in original order:");
-                        for (index = 0; index < numbers.Length; ++index)
-                            Write("{0,4}", numbers[index]);
-                        WriteLine("\n");
-                        break;
-                    case 2: //Display array in reverse order
-                        Write("View list in reverse order:");
-                        for (index = (numbers.Length - 1); index >= 0; --index)
-                            Write("{0,4}", numbers[index]);
-                        WriteLine("\n");
-                        break;
-                    case 3: //Prompt user for a position and output result
-                        do
+                    case 1: // Display array in original order
+                        WriteLine("View list in original order:");
+                        foreach (int number in numbers)
                         {
-                            WriteLine("Enter a position number to view (1 through {0}): \n", numbers.Length);
-                            int.TryParse(ReadLine(), out userInput);
-                        } while (userInput <= 0 || userInput > numbers.Length);
-                        WriteLine("The value at position {0} is {1}\n", userInput, numbers[userInput - 1]);
+                            Write($"{number,4}");
+                        }
+                        WriteLine();
+                        break;
+
+                    case 2: // Display array in reverse order
+                        WriteLine("View list in reverse order:");
+                        for (int i = numbers.Length - 1; i >= 0; i--)
+                        {
+                            Write($"{numbers[i],4}");
+                        }
+                        WriteLine();
+                        break;
+
+                    case 3: // Prompt user for a position and output result
+                        WriteLine("Enter a position number to view (1 through {0}): \n", numbers.Length);
+                        int position = int.Parse(ReadLine());
+
+                        if (position < 1 || position > numbers.Length)
+                        {
+                            continue;
+                        }
+
+                        WriteLine("The value at position {0} is {1}\n", position, numbers[position - 1]);
                         break;
                 }
 
-                //Prompt user for array display option
+                // Prompt user for array display option
                 WriteLine("Please enter one of the following options:\n" +
                     "(1) to view the list in original order.\n" +
                     "(2) to view the list in reverse order.\n" +
                     "(3) to choose a specific position to view.\n" +
                     "(4) to quit the application.\n");
 
-                //Convert input to integer
-                int.TryParse(ReadLine(), out userInput);
+                // Convert input to integer
+                userInput = int.Parse(ReadLine());
 
-            } while (userInput != EXIT); //Continue looping until user enters sentinel value
+            } while (userInput != EXIT); // Continue looping until user enters sentinel value
         }
     }
 }
